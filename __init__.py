31660b75e7d8e79908083cc1760b2c4f32feb1f6
prestonmoody <pmoody199@gmail.com>
2024-07-03 18:57:24 -0600

    15

--- __init__.py
+++ __init__.py
@@ -2,17 +2,14 @@ from flask import Flask
 # from flask_sqlalchemy import SQLAlchemy
 
 # db = SQLAlchemy()
+
 def create_app(env='dev'):
+    app = Flask(__name__)
 
-  app = Flask(__name__)
+    # app.config['SECRET_KEY'] = ''
+    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
-  # app.config['SECRET_KEY'] = ''
-  # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
-
-  # db.init_app(app)
-
-  if __name__ == '__main__':
-    app.run(debug=True)
+    # db.init_app(app)
 
     @app.route('/')
     def index():
@@ -21,5 +18,13 @@ def create_app(env='dev'):
     @app.route('/profile')
     def profile():
         return "Profile"
-  return app
+
+    if __name__ == '__main__':
+        app.run(debug=True)
+
+    return app
+
+# To run the application, you would typically create an instance of the app:
+# app = create_app()
+
  
